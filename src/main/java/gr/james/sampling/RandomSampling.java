66ac5dc00a4481ca701c8795ae3bc44d69354066
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-02-08 10:42:34 +0200

    Fix interface javadoc

--- src/main/java/gr/james/sampling/RandomSampling.java
+++ src/main/java/gr/james/sampling/RandomSampling.java
@@ -12,9 +12,11 @@ import java.util.Iterator;
  * subinterface of {@code RandomSampling}.
  * <p>
  * Classes that implement this interface must have a static method with signature
- * <pre>{@code public static <E> RandomSamplingCollector<E> collector(int sampleSize, Random random)}</pre>
+ * <pre><code>
+ * public static <E> RandomSamplingCollector<E> collector(int sampleSize, Random random)
+ * </code></pre>
  * that returns a {@link RandomSamplingCollector} to use with the Java 8 stream API.
- *
+ * <p>
  * Classes that implement this interface must have constant space complexity in respect to the stream size.
  *
  * @param <T> the item type
