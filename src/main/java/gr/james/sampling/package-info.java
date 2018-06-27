7194cb253d4f5e343deba0e28d70db0df60c0e7c
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-06-27 20:17:31 +0300

    Add package-info.java for gr.james.sampling

---
+++ src/main/java/gr/james/sampling/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * The package containing the utilities for random sampling.
+ * <p>
+ * The top level interfaces are {@link gr.james.sampling.RandomSampling} and
+ * {@link gr.james.sampling.WeightedRandomSampling}, which represent unweighted and weighted random sampling algorithms
+ * respectively.
+ * <h3><code>RandomSampling</code> implementations</h3>
+ * <ul>
+ * <li>{@link gr.james.sampling.WatermanSampling}</li>
+ * <li>{@link gr.james.sampling.VitterXSampling}</li>
+ * <li>{@link gr.james.sampling.VitterZSampling}</li>
+ * </ul>
+ * <h3><code>WeightedRandomSampling</code> implementations</h3>
+ * <ul>
+ * <li>{@link gr.james.sampling.ChaoSampling}</li>
+ * <li>{@link gr.james.sampling.EfraimidisSampling}</li>
+ * </ul>
+ */
+package gr.james.sampling;
