9d3ca312e057cf6e3a67c9bf4b76463b172b647f
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-06-29 20:34:21 +0300

    Rename SelectRandomFromStream demo to SelectRandomFromStreamX

--- demo/src/SelectRandomFromStreamX.java
+++ demo/src/SelectRandomFromStreamX.java
@@ -8,10 +8,9 @@ import java.util.Scanner;
 /**
  * Select 5 random tokens from an input stream using {@link VitterXSampling}.
  */
-public final class SelectRandomFromStream {
+public final class SelectRandomFromStreamX {
     public static void main(String[] args) {
         RandomSampling<String> rs = new VitterXSampling<>(5, new Random());
-        System.out.println(Charset.defaultCharset().name());
         rs.feed(new Scanner(System.in, Charset.defaultCharset().name()));
         System.out.println(rs.sample());
     }
