16e574df4cd351ffe005391736b9a1c20c0251ed
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-07-03 01:09:39 +0300

    SkipFunction accepts the stream size

--- src/main/java/gr/james/sampling/SkipFunction.java
+++ src/main/java/gr/james/sampling/SkipFunction.java
@@ -1,15 +1,19 @@
 package gr.james.sampling;
 
 /**
- * A skip function returns how many elements to skip.
+ * A skip function returns how many elements a reservoir algorithm must skip before accepting an element in the
+ * reservoir.
  */
 @Deprecated
 @FunctionalInterface
 interface SkipFunction {
     /**
      * Returns a {@code long} indicating how many elements the algorithm must skip.
+     * <p>
+     * This method is called right after an element was accepted in the reservoir.
      *
+     * @param streamSize the stream size
      * @return how many elements the algorithm must skip
      */
-    long skip();
+    long skip(long streamSize);
 }
