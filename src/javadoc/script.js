c425b738dac00d243487252ad38e997db05596c3
Giorgos Stamatelatos <gstamatelat@gmail.com>
2018-07-04 16:10:25 +0300

    Fix external links in new tab (#25)

--- src/javadoc/script.js
+++ src/javadoc/script.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     var anchors = document.getElementsByTagName('a');
     var length = anchors.length;
     for (var i = 0; i < length; i++) {
-        if (anchors[i].href.indexOf('http://') === 0 || anchors[i].href.indexOf('https://') === 0) {
-            anchors[i].target = '_blank';
+        if (location.protocol === 'http:' || location.protocol === 'https:') {
+            if (anchors[i].href.indexOf(location.origin) !== 0) {
+                anchors[i].target = '_blank';
+            }
+        } else {
+            if (anchors[i].href.indexOf('file:') !== 0) {
+                anchors[i].target = '_blank';
+            }
         }
     }
 }, false);
