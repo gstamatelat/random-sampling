addb614c3bc4a247fa0878e339fe43797a4b9006
Giorgos Stamatelatos <gstamatelat@gmail.com>
2022-09-04 22:02:44 +0300

    Multiple iterations per implementation

--- src/test/java/gr/james/sampling/Benchmark.java
+++ src/test/java/gr/james/sampling/Benchmark.java
@@ -1,6 +1,7 @@
 package gr.james.sampling;
 
 import gr.james.sampling.implementations.*;
+import gr.james.stats.utils.WelfordVariance;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,7 @@ public class Benchmark {
     private static final Random random = new Random();
     private static final int sample = 10;
     private static final int stream = 100000000;
+    private static final int reps = 5;
 
     private static final List<RandomSamplingImplementation<Object>> implementations = Arrays.asList(
             new WatermanImplementation<>(),
@@ -26,7 +28,11 @@ public class Benchmark {
 
     public static void main(String[] args) {
         for (RandomSamplingImplementation<Object> impl : implementations) {
-            System.out.printf("%18s %5d ms%n", impl.toString(), singlePerformance(impl) / 1000000);
+            final WelfordVariance wv = new WelfordVariance();
+            for (int rep = 0; rep < reps; rep++) {
+                wv.add(singlePerformance(impl) / 1000000.0);
+            }
+            System.out.printf("%18s %.0f ms%n", impl, wv.mean());
         }
     }
 
