c0ed86f92833ea7ec7146e0f6c2fc341818487d6
Giorgos Stamatelatos <gstamatelat@gmail.com>
2022-09-04 22:53:44 +0300

    Use fixed number of iterations

--- src/test/java/gr/james/sampling/Benchmark.java
+++ src/test/java/gr/james/sampling/Benchmark.java
@@ -1,7 +1,6 @@
 package gr.james.sampling;
 
 import gr.james.sampling.implementations.*;
-import gr.james.stats.utils.WelfordVariance;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +11,7 @@ public class Benchmark {
     private static final Random random = new Random();
     private static final int sample = 10;
     private static final int stream = 100000000;
-    private static final int reps = 5;
+    private static final int reps = 10;
 
     private static final List<RandomSamplingImplementation<Object>> implementations = Arrays.asList(
             new WatermanImplementation<>(),
@@ -28,11 +27,11 @@ public class Benchmark {
 
     public static void main(String[] args) {
         for (RandomSamplingImplementation<Object> impl : implementations) {
-            final WelfordVariance wv = new WelfordVariance();
+            double sum = 0;
             for (int rep = 0; rep < reps; rep++) {
-                wv.add(singlePerformance(impl) / 1000000.0);
+                sum += singlePerformance(impl) / 1000000.0;
             }
-            System.out.printf("%18s %.0f ms%n", impl, wv.mean());
+            System.out.printf("%18s %.0f ms%n", impl, sum / reps);
         }
     }
 
